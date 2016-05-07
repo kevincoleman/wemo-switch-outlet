5f617727cfdeddde1778a81ac236ef0b84e658d8
Kevin N. Coleman <kevin.n.coleman@gmail.com>
2016-05-07 00:22:08 -0700

    refactor; retry
    Lumped client creations into the same callback; added retry after 3
    seconds; cleanup

--- app.js
+++ app.js
@@ -11,38 +11,39 @@ async.waterfall([
   function(callback){
     function foundDevice(device) {
       if(device.friendlyName === lightSwitchName) {
-        var lightSwitch = device;
         var lightSwitchClient = this.client(device);
-        callback(null, lightSwitchClient);
       }
-    };
-    wemo.discover(foundDevice);
-  },
-  function(lightSwitchClient, callback){
-    function foundDevice(device) {
       if(device.friendlyName === outletSwitchName) {
-        var outletSwitch = device;
         var outletSwitchClient = this.client(device);
-        callback(null, [lightSwitchClient, outletSwitchClient]);
-        lightSwitchClient.on('binaryState', function(value) {
-          outletSwitchClient.setBinaryState(value);
-        });
       }
+      setTimeout(function(){
+        if (
+          (typeof lightSwitchClient !== 'undefined') &&
+          (typeof outletSwitchClient !== 'undefined')
+        ) {
+          callback(null, [lightSwitchClient, outletSwitchClient]);
+        } else {
+          console.log("Failed to find device(s). Retrying...");
+          wemo.discover(foundDevice);
+        }
+      }, 3000);
     };
     wemo.discover(foundDevice);
   },
   function(clients, callback){
     var lightSwitchClient = clients[0];
     var outletSwitchClient = clients[1];
+    
+    lightSwitchClient.on('binaryState', function(value) {
+      outletSwitchClient.setBinaryState(value);
+    });
     outletSwitchClient.on('binaryState', function(value) {
       lightSwitchClient.setBinaryState(value);
     });
-   callback(null, clients); 
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Switch and Outlet Linked. Go play outside.');
+    }
   }
-], function(err, clients){
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Switch and Outlet Linked. Go play outside.');
-  }
-});
+]);
