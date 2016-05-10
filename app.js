50eb589c2e17f1b1fd40d8dba4f760d6958d2381
kevincoleman <kevin.n.coleman@gmail.com>
2016-05-09 21:29:54 -0700

    fixing clientCheck

--- app.js
+++ app.js
@@ -3,54 +3,59 @@ var async              = require('async');
 
 var lightSwitchName    = "Front Porch Switch";
 var outletSwitchName   = "Front Porch";
+
+var lightSwitchClient;
+var outletSwitchClient;
+
 var wemo               = new Wemo();
 
 
 async.waterfall([
   function(callback){
-  
-    var clientCheck = function() {
-      setTimeout(function(){
-        if (
-          (typeof this.lightSwitchClient !== 'undefined') &&
-          (typeof this.outletSwitchClient !== 'undefined')
-        ) {
-          callback(null, [this.lightSwitchClient, this.outletSwitchClient]);
-        } else {
-          console.log("Failed to find device(s). Retrying...");
+
+    var clientCheck = function(foundDevice) {
+      if (
+        ((lightSwitchClient !== undefined) && (outletSwitchClient !== undefined))
+      ) {
+        console.log('Both found, progressing.');
+        callback(null, [lightSwitchClient, outletSwitchClient]);
+      } else {
+        console.log("Failed to find device(s). Retrying...");
+        setTimeout(function(){
           wemo.discover(foundDevice);
-        }
-      }, 3000);
-    }.bind(this);
-    
+        }, 3000);
+      }
+    }
+
     var foundDevice = function(device) {
+      console.log(device.friendlyName);
       if(device.friendlyName === lightSwitchName) {
         console.log(lightSwitchName + " found.");
-        this.lightSwitchClient = this.client(device);
-        this.clientCheck();
+        lightSwitchClient = wemo.client(device);
+        clientCheck();
       }
       if(device.friendlyName === outletSwitchName) {
         console.log(outletSwitchName + " found.");
-        this.outletSwitchClient = this.client(device);
-        this.clientCheck();
+        outletSwitchClient = wemo.client(device);
+        clientCheck();
       }
-    }.bind(this);
-    
+    };
+    console.log('start');
     wemo.discover(foundDevice);
-    
+
   },
   function(clients, callback){
-  
+
     var lightSwitchClient = clients[0];
     var outletSwitchClient = clients[1];
-    
+
     lightSwitchClient.on('binaryState', function(value) {
       outletSwitchClient.setBinaryState(value);
     });
     outletSwitchClient.on('binaryState', function(value) {
       lightSwitchClient.setBinaryState(value);
     });
-    
+
     console.log('Switch and Outlet Linked. Go play outside.');
   }
 ]);
